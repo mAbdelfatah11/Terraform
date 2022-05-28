a97657dda98c1ca1d5fa254d1a4f73afcb6f948c
mahmoud.abdelfatah707@gmail.com <mahmoud.abdelfatah707@gmail.com>
2022-05-28 19:26:40 +0200

    upload clean files before demos

--- 1__Demo-1__create-aws-IAC-for-dockerized-app/entry-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum update -y && sudo yum install -y docker
-sudo systemctl start docker 
-sudo usermod -aG docker ec2-user
-docker pull mabdelfatah/voting-app-py:v0.0.1
-docker run -ti -v /tmp:/tmp -dp 8080:80 mabdelfatah/voting-app-py:v0.0.1
